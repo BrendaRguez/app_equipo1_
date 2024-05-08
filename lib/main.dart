b18eb5d06a60e2d808e6e2a9bb4c730e2794be16
KennyLC <kennyalcarrillo@gmail.com>
2024-05-07 20:39:27 -0600

    Abrir otro socket de comunicación

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 //Código que ya se actualiza. :)
 import 'dart:math';
-import 'dart:convert'; 
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -12,22 +12,25 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key}); //IDK, quitar si se puede
-  
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
         title: "First App",
-        theme: ThemeData(useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor:const  Color.fromARGB(255, 5, 29, 90)),),
-          home: MyHomePage(),
-    ),);
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 5, 29, 90)),
+        ),
+        home: MyHomePage(),
+      ),
+    );
   }
 }
 
 class MyAppState extends ChangeNotifier {
-
   String receivedData = '';
   Socket? socket;
   Socket? socket2;
@@ -46,15 +49,14 @@ class MyAppState extends ChangeNotifier {
     return jsonDecode(str);
   }
 
-  void save_time(dynamic data){
+  void save_time(dynamic data) {
     //print(data.length);
-    
+
     time = decodeList(data);
     //notifyListeners();
   }
 
-  void save_disp(dynamic data){
-    
+  void save_disp(dynamic data) {
     disp = decodeList(data);
     //print(data.length);
     print("Disp length----");
@@ -62,18 +64,25 @@ class MyAppState extends ChangeNotifier {
     print("Time length----");
     print(time.length);
     notifyListeners();
-
   }
 
   void _startSocketCommunication() async {
-      //while(true){
-      // Connect to MicroPython Access Point
-      socket = await Socket.connect('192.168.4.1', 80);
-      socket!.add(utf8.encode("---------Flag------------"));
-      socket!.close();
-      await Future.delayed(const Duration(milliseconds: 100));
-  }
+    //while(true){
+    // Connect to MicroPython Access Point
+    socket = await Socket.connect('192.168.4.1', 80);
+    socket!.add(utf8.encode("---------Flag------------"));
+    socket!.close();
+    await Future.delayed(const Duration(milliseconds: 100));
+    Socket socket3 = await Socket.connect('192.168.4.1', 8080);
+    List<int> receivedData_time = [];
 
+    socket3.listen(
+      //TIME DATA
+      (List<int> data) {
+        receivedData_time.addAll(data);
+      },
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
@@ -82,7 +91,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
   List<FlSpot> generateRandomData(double count) {
     List<FlSpot> data = [];
     for (int i = 0; i < 100; i++) {
@@ -94,18 +102,18 @@ class _MyHomePageState extends State<MyHomePage> {
   List<FlSpot> createdata(List data) {
     List<FlSpot> result = [];
     for (int i = 0; i < data.length; i++) {
-      result.add(FlSpot(i.toDouble(), data[i].toDouble())); // Random value between 0 and 100
+      result.add(FlSpot(
+          i.toDouble(), data[i].toDouble())); // Random value between 0 and 100
     }
     return result;
   }
 
   @override
   Widget build(BuildContext context) {
-
     var appState = context.watch<MyAppState>();
     List<dynamic> disp = appState.disp;
     //List<FlSpot> displacement = [];
-   // List<dynamic> originalList = [1, 2, 3, 4, 5]; // Example list
+    // List<dynamic> originalList = [1, 2, 3, 4, 5]; // Example list
     List<FlSpot> displacement = <FlSpot>[];
     displacement = createdata(disp);
 
@@ -142,16 +150,14 @@ class _MyHomePageState extends State<MyHomePage> {
             const SizedBox(height: 100),
             ElevatedButton(
               onPressed: () {
-                 appState._startSocketCommunication();
-                 //updateGraphData();
-               },
+                appState._startSocketCommunication();
+                //updateGraphData();
+              },
               child: const Text('Update Data'),
             ),
           ],
         ),
       ),
     );
-
-
   }
 }
